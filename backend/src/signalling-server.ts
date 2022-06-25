679b928ceebfab2f5e47710c2fbff6cd32f52dc9
illarionov-company <lavrikanec@gmail.com>
2022-06-25 16:42:10 +0900

    ts-node fixed

--- backend/src/signalling-server.ts
+++ backend/src/signalling-server.ts
@@ -1,14 +1,16 @@
+import dotenv from "dotenv"
 import { readFileSync } from "fs"
 import { createServer } from "http"
 import { randomBytes } from "crypto"
 import { WebSocketServer } from "ws"
+import path from "path"
+
+import { ERRORS } from "../../common/constants/ERRORS"
+import { SIGNALS } from "../../common/constants/SIGNALS"
 
 import { Player, playerId } from "./classes/Player"
 import { Room } from "./classes/Room"
 
-import { ERRORS } from "../../_constants/ERRORS"
-import { SIGNALS } from "../../_constants/SIGNALS"
-
-const server = createServer()
-
-server.listen(80)
+dotenv.config({
+	path: path.join(__dirname, "../../common/env/.env." + process.env.NODE_ENV),
+})
